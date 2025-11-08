cb9c5b122625d57290405877d0efc62692895d2f
Piper Inns Hall <piper.innshall@gmail.com>
2025-11-08 17:55:31 +1300

    feat: Border

--- scripts/map_generation/map_settings.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name MapSettings
-
-@export_category("Map Settings")
-
-@export var width: int = 512
-@export var height: int = 512
-
-@export var noise_type: FastNoiseLite.NoiseType
